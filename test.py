739d71695a45d5fc158aca4e43bee044dc4ce753
Kimseonghun-468 <tjdgns4560@naver.com>
2021-05-26 16:52:38 +0900

    Detection

---
+++ test.py
@@ -0,0 +1,141 @@
+import cv2, dlib
+import numpy as np
+from keras.preprocessing.image import img_to_array
+from imutils import face_utils, resize
+from keras.models import load_model
+
+IMG_SIZE = (34, 26)
+
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
+
+model = load_model('models/2018_12_17_22_58_35.h5')
+model.summary()
+
+detection_model_path = 'haarcascade_frontalface_default.xml'
+emotion_model_path = 'models/_mini_XCEPTION.102-0.66.hdf5'
+face_detection = cv2.CascadeClassifier(detection_model_path)
+emotion_classifier = load_model(emotion_model_path, compile=False)
+EMOTIONS = ["angry" ,"disgust","scared", "happy", "sad", "surprised",
+ "neutral"]
+
+def crop_eye(img, eye_points):
+    x1, y1 = np.amin(eye_points, axis=0)
+    x2, y2 = np.amax(eye_points, axis=0)
+    cx, cy = (x1 + x2) / 2, (y1 + y2) / 2
+
+    w = (x2 - x1) * 1.2
+    h = w * IMG_SIZE[1] / IMG_SIZE[0]
+
+    margin_x, margin_y = w / 2, h / 2 
+
+    min_x, min_y = int(cx - margin_x), int(cy - margin_y)
+    max_x, max_y = int(cx + margin_x), int(cy + margin_y)
+
+    eye_rect = np.rint([min_x, min_y, max_x, max_y]).astype(np.int)
+
+    eye_img = gray[eye_rect[1]:eye_rect[3], eye_rect[0]:eye_rect[2]]
+
+    return eye_img, eye_rect
+
+# main
+cap = cv2.VideoCapture(0)
+#cap = cv2.VideoCapture('videos/2.mp4')
+
+while True:
+    ret, img_ori = cap.read()
+    frame = resize(img_ori,width=300)
+    if not ret:
+        break
+    
+  
+  
+    img_ori = cv2.resize(img_ori, dsize=(0, 0), fx=0.5, fy=0.5)
+    img = img_ori.copy()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+  
+    
+    faces_1 = face_detection.detectMultiScale(gray,scaleFactor=1.1,minNeighbors=5,minSize=(30,30),flags=cv2.CASCADE_SCALE_IMAGE)
+    canvas = np.zeros((250, 300, 3), dtype="uint8")
+    frameClone = frame.copy()
+    if len(faces_1) > 0:
+        faces_1 = sorted(faces_1, reverse=True,
+        key=lambda x: (x[2] - x[0]) * (x[3] - x[1]))[0]
+        (fX, fY, fW, fH) = faces_1
+                    # Extract the ROI of the face from the grayscale image, resize it to a fixed 28x28 pixels, and then prepare
+            # the ROI for classification via the CNN
+        roi = gray[fY:fY + fH, fX:fX + fW]
+        roi = cv2.resize(roi, (64, 64))
+        roi = roi.astype("float") / 255.0
+        roi = img_to_array(roi)
+        roi = np.expand_dims(roi, axis=0)
+        
+        preds = emotion_classifier.predict(roi)[0]
+        emotion_probability = np.max(preds)
+        label = EMOTIONS[preds.argmax()]
+    else: continue
+        
+    for (i, (emotion, prob)) in enumerate(zip(EMOTIONS, preds)):
+        # construct the label text
+        text = "{}: {:.2f}%".format(emotion, prob * 100)#
+
+        # draw the label + probability bar on the canvas
+        # emoji_face = feelings_faces[np.argmax(preds)]
+
+
+        w = int(prob * 300)
+        cv2.rectangle(canvas, (7, (i * 35) + 5),
+        (w, (i * 35) + 35), (0, 0, 255), -1)
+        cv2.putText(canvas, text, (10, (i * 35) + 23),
+        cv2.FONT_HERSHEY_SIMPLEX, 0.45,
+        (255, 255, 255), 2)
+        cv2.putText(frameClone, label, (fX, fY - 10),
+        cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+        cv2.rectangle(frameClone, (fX, fY), (fX + fW, fY + fH),
+                      (0, 0, 255), 2)
+
+    faces = detector(gray)
+
+    for face in faces:
+        shapes = predictor(gray, face)
+        shapes = face_utils.shape_to_np(shapes)
+
+        eye_img_l, eye_rect_l = crop_eye(gray, eye_points=shapes[36:42])
+        eye_img_r, eye_rect_r = crop_eye(gray, eye_points=shapes[42:48])
+
+        eye_img_l = cv2.resize(eye_img_l, dsize=IMG_SIZE)
+        eye_img_r = cv2.resize(eye_img_r, dsize=IMG_SIZE)
+        eye_img_r = cv2.flip(eye_img_r, flipCode=1)
+
+#        cv2.imshow('l', eye_img_l)
+#        cv2.imshow('r', eye_img_r)
+
+        eye_input_l = eye_img_l.copy().reshape((1, IMG_SIZE[1], IMG_SIZE[0], 1)).astype(np.float32) / 255.
+        eye_input_r = eye_img_r.copy().reshape((1, IMG_SIZE[1], IMG_SIZE[0], 1)).astype(np.float32) / 255.
+
+        pred_l = model.predict(eye_input_l)
+        pred_r = model.predict(eye_input_r)
+
+        # visualize
+        state_l = 'O %.1f' if pred_l > 0.1 else '- %.1f'
+        state_r = 'O %.1f' if pred_r > 0.1 else '- %.1f'
+
+        state_l = state_l % pred_l
+        state_r = state_r % pred_r
+
+        cv2.rectangle(img, pt1=tuple(eye_rect_l[0:2]), pt2=tuple(eye_rect_l[2:4]), color=(255,255,255), thickness=2)
+        cv2.rectangle(img, pt1=tuple(eye_rect_r[0:2]), pt2=tuple(eye_rect_r[2:4]), color=(255,255,255), thickness=2)
+        cv2.putText(img, label, (fX, fY - 10),
+        cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+        cv2.putText(img, state_l, tuple(eye_rect_l[0:2]), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255,255,255), 2)
+        cv2.putText(img, state_r, tuple(eye_rect_r[0:2]), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255,255,255), 2)
+
+#    cv2.imshow('your_face', frameClone)
+#    cv2.imshow("Probabilities", canvas)
+    cv2.imshow('result', img)
+    if cv2.waitKey(1) == ord('q'):
+        break
+        
+camera.release()
+cv2.destroyAllWindows()
+
